49e2bf0f518eb8c22f740f24a7449e69a8bf9f55
Satyam Mishra <satyyam.misshra1121@gmail.com>
2025-04-08 11:39:32 +0530

    new api added

--- quiz-app/src/main/java/com/example/quiz/app/controllers/UserController.java
+++ quiz-app/src/main/java/com/example/quiz/app/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.quiz.app.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +27,12 @@ public class UserController {
         return userRepository.findById(id)
                 .map(user -> ResponseEntity.ok(user))
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
 
